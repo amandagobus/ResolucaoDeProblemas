784ea02beafb5ece4b25dea34601a5a4ef3deec3
amandagobus <amandagobus@gmail.com>
2016-10-18 19:40:19 -0200

    Alteração na classe ListaDeImoveis
    Adicionei os metodos de Ler Apartamento e ler Casa

--- RP2t3/src/ListaImoveis/ListaDeImoveis.java
+++ RP2t3/src/ListaImoveis/ListaDeImoveis.java
@@ -5,6 +5,7 @@
  */
 package ListaImoveis;
 
+import Apartamento.Apartamento;
 import Chacara.Chacara;
 import Imovel.Imovel;
 import SalaComercial.SalaComercial;
@@ -20,6 +21,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import static java.lang.Integer.parseInt;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -88,23 +90,28 @@ public class ListaDeImoveis implements ListaImoveis {
     public List<Imovel> ordenarArea() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
     /**
      * Metodo que pesquisa um Imovel pelo valor
+     *
      * @param bairro, inoformado pelo usuario
-     * @return uma lista de Imóvel, cujo valor seja menor ou igual ao valor 
+     * @return uma lista de Imóvel, cujo valor seja menor ou igual ao valor
      * informado
      */
     @Override
     public List<Imovel> pesquisaValor(double valor) {
         List<Imovel> l = new ArrayList<>();
         for (Imovel imovel : lista) {
-            if(imovel.getValor()<= valor)
+            if (imovel.getValor() <= valor) {
                 l.add(imovel);
-        }return l;
+            }
+        }
+        return l;
     }
 
     /**
      * Metodo que pesquisa um Imovel pelo bairro
+     *
      * @param bairro, inoformado pelo usuario
      * @return uma lista de Imóvel
      */
@@ -112,41 +119,41 @@ public class ListaDeImoveis implements ListaImoveis {
     public List<Imovel> pesquisaBairro(String bairro) {
         List<Imovel> l = new ArrayList<>();
         for (Imovel imovel : lista) {
-            if(imovel.getBairro().contains(bairro))
+            if (imovel.getBairro().contains(bairro)) {
                 l.add(imovel);
-        }return l;
-        
-   }
+            }
+        }
+        return l;
 
-    
+    }
+
     @Override
     public boolean lerArquivo() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
     @Override
-    public boolean escreverArquivo(){
+    public boolean escreverArquivo() {
 
-   try{        
-        FileWriter outFile = new FileWriter(new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "Sala.csv"));
-        BufferedWriter escrever = new BufferedWriter(outFile);
-        Imovel mo = lista.get(0);
-        escrever.write(mo.toFileTitulo());
-        escrever.write("\r\n");
-
-        
-        for (Imovel imovel : lista) {
-            escrever.write(imovel.toFile());
+        try {
+            FileWriter outFile = new FileWriter(new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "Sala.csv"));
+            BufferedWriter escrever = new BufferedWriter(outFile);
+            Imovel mo = lista.get(0);
+            escrever.write(mo.toFileTitulo());
             escrever.write("\r\n");
-        } 
-        escrever.close();
-        outFile.close();
-        return true;
-   }catch(IOException ex){
-  Logger.getLogger(MenuSalaComercial.class.getName()).log(Level.SEVERE, null, ex);
-   
-   } return false;
+
+            for (Imovel imovel : lista) {
+                escrever.write(imovel.toFile());
+                escrever.write("\r\n");
+            }
+            escrever.close();
+            outFile.close();
+            return true;
+        } catch (IOException ex) {
+            Logger.getLogger(MenuSalaComercial.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+        return false;
     }
 
     public boolean ler() throws FileNotFoundException, IOException {
@@ -262,7 +269,7 @@ public class ListaDeImoveis implements ListaImoveis {
         }
         return false;
     }
-    */
+     */
     public void gravarChacara() throws Exception {
 
         //verificar se o arquivo existe, se não existeir criar (o ato de recriar o mesmo arquivo ja resolve por se só?)
@@ -275,7 +282,7 @@ public class ListaDeImoveis implements ListaImoveis {
         // criar um metodo nas class abstratas Filewhite. 
         for (Imovel imovel : lista) {
             escrever.write(imovel.getCodigo() + "," + imovel.getLogradouro() + "," + imovel.getNumero()
-                    + "," + imovel.getBairro() + "," + imovel.getCidade() + "," + imovel.getDescricao() + "," + imovel.getAreaTotal() + "," + imovel.getValor() 
+                    + "," + imovel.getBairro() + "," + imovel.getCidade() + "," + imovel.getDescricao() + "," + imovel.getAreaTotal() + "," + imovel.getValor()
                     + "," + imovel.getAreaConstruida() + "," + imovel.getNumeroQuartos() + "," + imovel.getAnoConstrucao()
                     + "," + imovel.getDistCidade());
             escrever.write("\r\n");
@@ -285,7 +292,7 @@ public class ListaDeImoveis implements ListaImoveis {
         outFile.close();
 
     }
-    
+
     public boolean lerChacara() throws FileNotFoundException, IOException {
 
         File file = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "Chacara.csv");
@@ -316,11 +323,9 @@ public class ListaDeImoveis implements ListaImoveis {
                 anoConstrucao = Integer.parseInt(parte[10]);
                 distCidade = Double.parseDouble(parte[11]);
 
-                
-                
-            chacara = new Chacara(distCidade, logradouro, numero, bairro, cidade, 
-            descricao, areaTotal, valor, areaConstruida, numeroQuartos, anoConstrucao);
-                
+                chacara = new Chacara(distCidade, logradouro, numero, bairro, cidade,
+                        descricao, areaTotal, valor, areaConstruida, numeroQuartos, anoConstrucao);
+
                 incluir(chacara);
 
             }
@@ -331,4 +336,110 @@ public class ListaDeImoveis implements ListaImoveis {
         }
         return false;
     }
+
+    public boolean lerApartamento() throws FileNotFoundException, IOException {
+
+        File file = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "Apartamento.csv");
+
+        if (file.exists()) {
+            FileInputStream arquivo;
+            BufferedReader ler;
+            String linha, logradouro, bairro, cidade, descricao, nomeEdificio;
+            int codigo, numero, andar;
+            double areaTotal, valor, valorCondominio;
+            int numeroQuartos;
+            int numeroVagas;
+            int anoDeConstrucao;
+            int numeroDoApartamento;
+
+            Imovel sala;
+            arquivo = new FileInputStream(new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "Sala.csv"));
+            ler = new BufferedReader(new InputStreamReader(arquivo, "UTF-8"));
+
+            linha = ler.readLine();
+            while ((linha = ler.readLine()) != null) {
+                String parte[] = linha.split(",");
+                codigo = Integer.parseInt(parte[0]);
+                logradouro = parte[1];
+                numero = Integer.parseInt(parte[2]);
+                bairro = parte[3];
+                cidade = parte[4];
+                descricao = parte[5];
+                areaTotal = Double.parseDouble(parte[6]);
+                valor = Double.parseDouble(parte[7]);
+
+                nomeEdificio = parte[8];
+                andar = Integer.parseInt(parte[9]);
+                valorCondominio = Double.parseDouble(parte[10]);
+
+                numeroQuartos = Integer.parseInt(parte[11]);
+                numeroVagas = Integer.parseInt(parte[12]);
+                anoDeConstrucao = Integer.parseInt(parte[13]);
+                numeroDoApartamento = Integer.parseInt(parte[14]);
+
+                Apartamento ap = new Apartamento(logradouro, numero, bairro, cidade,
+                        descricao, areaTotal, valor, nomeEdificio, andar,
+                        valorCondominio, numeroQuartos, numeroVagas, anoDeConstrucao, numeroDoApartamento);
+                incluir(ap);
+
+            }
+            ler.close();
+            arquivo.close();
+            return true;
+
+        }
+        return false;
+    }
+
+    public boolean lerCasa() throws FileNotFoundException, IOException {
+
+        File file = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "Sala.csv");
+
+        if (file.exists()) {
+            FileInputStream arquivo;
+            BufferedReader ler;
+            String linha, logradouro, bairro, cidade, descricao;
+            int codigo, numero;
+            double areaTotal, valor;
+
+            int tipo;
+            double areaConstruida;
+            int numeroQuartos;
+            int anoConstrucao;
+
+            int numeroDeVagas;
+
+            Imovel sala;
+            arquivo = new FileInputStream(new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "Casa.csv"));
+            ler = new BufferedReader(new InputStreamReader(arquivo, "UTF-8"));
+
+            linha = ler.readLine();
+            while ((linha = ler.readLine()) != null) {
+                String parte[] = linha.split(",");
+                codigo = Integer.parseInt(parte[0]);
+                logradouro = parte[1];
+                numero = Integer.parseInt(parte[2]);
+                bairro = parte[3];
+                cidade = parte[4];
+                descricao = parte[5];
+                areaTotal = Double.parseDouble(parte[6]);
+                valor = Double.parseDouble(parte[7]);
+                areaConstruida = Double.parseDouble(parte[8]);
+                numeroQuartos = Integer.parseInt(parte[9]);
+                anoConstrucao = Integer.parseInt(parte[10]);
+                numeroDeVagas = Integer.parseInt(parte[11]);
+                tipo = parseInt(parte[12]);
+
+                Apartamento ap = new Apartamento(logradouro, numero, bairro, cidade, descricao, areaTotal, valor, descricao, tipo, valor, numeroDeVagas, anoConstrucao, numeroDeVagas, numeroQuartos);
+                incluir(ap);
+
+            }
+            ler.close();
+            arquivo.close();
+            return true;
+
+        }
+        return false;
+    }
+
 }
